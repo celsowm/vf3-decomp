0bbd92c1230aa75eac99199d692aef4415bd26c7
celsowm <celsowm@gmail.com>
2026-09-25 10:53:36 -0300

    Follow-up: poly_classify ram-map OOB counter (shared by vecpush replay) + tranche-2 watch list

--- src/fight/poly_classify.h
+++ src/fight/poly_classify.h
@@ -7,7 +7,7 @@
 #include "fight/orient2.h"
 
 typedef struct {
-    const uint8_t *data; /* window bytes */
+    uint8_t *data;       /* window bytes (writable shadow for replay) */
     uint32_t base;       /* canonical P2 address of data[0] */
     uint32_t len;        /* window length in bytes */
 } vf3_ram_win;
@@ -15,6 +15,7 @@ typedef struct {
 typedef struct {
     const vf3_ram_win *wins;
     int n;
+    uint32_t oob;        /* out-of-window access counter (replay check) */
 } vf3_ram_map;
 
 int vf3_poly_classify(uint32_t r4, uint32_t r3, float fr4, float fr5,
